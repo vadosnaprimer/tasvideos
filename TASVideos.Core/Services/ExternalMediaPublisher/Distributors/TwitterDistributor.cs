a4cf602cbbf129b11391d90147127959c8d3ca7b
Adam Michaud <invariel@gmail.com>
2022-01-08 19:45:40 -0500

    Twitter's v2 API requires a lot less bullshit to use, but it also requires refresh tokens.  Haven't written that logic yet.

--- TASVideos.Core/Services/ExternalMediaPublisher/Distributors/TwitterDistributor.cs
+++ TASVideos.Core/Services/ExternalMediaPublisher/Distributors/TwitterDistributor.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Json;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using TASVideos.Core.HttpClientExtensions;
@@ -16,7 +18,7 @@ namespace TASVideos.Core.Services.ExternalMediaPublisher.Distributors
 		private readonly AppSettings.TwitterConnection _settings;
 		private readonly ILogger<TwitterDistributor> _logger;
 
-		private static readonly Random Rng = new ();
+		private static readonly Random Rng = new();
 
 		public TwitterDistributor(
 			AppSettings appSettings,
@@ -37,108 +39,51 @@ namespace TASVideos.Core.Services.ExternalMediaPublisher.Distributors
 				return;
 			}
 
-			// Generate the Twitter message letting Twitter take care of the Embed from Open Graph Metadata
-			string twitterMessage = GenerateTwitterMessage(post);
+			TwitterPost twitterPost = GenerateTwitterMessage(post);
 
-			string nonce = GenerateNonce();
-			string timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
-
-			string signatureBaseString = CalculateSignatureBaseString("POST", $"{_settings.ApiBase}statuses/update.json", nonce, timestamp, twitterMessage);
-			string signature = CalculateSignature(signatureBaseString);
-
-			string oathToken = CalculateOAuthAuthorizationString(nonce, timestamp, signature);
-			_client.SetOAuthToken(oathToken);
-
-			var formFields = new List<KeyValuePair<string?, string?>>
+			if (twitterPost.Text != null)
 			{
-				new ("status", twitterMessage)
-			};
+				_client.SetBearerToken(_settings.AccessToken);
+				var response = await _client.PostAsJsonAsync("tweets", twitterPost);
 
-			var response = await _client.PostAsync("statuses/update.json", new FormUrlEncodedContent(formFields));
-
-			if (!response.IsSuccessStatusCode)
-			{
-				_logger.LogError($"[{DateTime.Now}] An error occurred sending a message to Twitter.");
-				_logger.LogError($"Signature Base String: {signatureBaseString}");
-				_logger.LogError($"Signature: {signature}");
-				_logger.LogError(_client.DefaultRequestHeaders.Authorization?.ToString());
-				_logger.LogError(await response.Content.ReadAsStringAsync());
+				if (!response.IsSuccessStatusCode)
+				{
+					if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized ||
+						response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+					{
+						// Try again after refreshing the access token.
+					}
+					else
+					{
+						_logger.LogError($"[{DateTime.Now}] An error occurred sending a message to Twitter.");
+						_logger.LogError(await response.Content.ReadAsStringAsync());
+					}
+				}
 			}
 		}
 
-		private static string GenerateTwitterMessage(IPostable post)
+		private static TwitterPost GenerateTwitterMessage(IPostable post)
 		{
 			return post.Group switch
 			{
-				PostGroups.Submission => $"{post.Announcement}\n{post.Link}",
-				_ => ""
+				PostGroups.Submission => new TwitterPost(post),
+				_ => new TwitterPost("")
 			};
 		}
 
-		private string CalculateSignatureBaseString(string method, string url, string nonceString, string timestamp, string statusMessage)
+		private class TwitterPost
 		{
-			string baseString = $"{method.ToUpper()}&{Uri.EscapeDataString(url)}";
+			public string Text { get; set; }
 
-			string consumerKey = UrlEncode("oauth_consumer_key", _settings.ConsumerKey);
-			string nonce = UrlEncode("oauth_nonce", nonceString);
-			string signatureMethod = UrlEncode("oauth_signature_method", "HMAC-SHA1");
-			string oauthTimestamp = UrlEncode("oauth_timestamp", timestamp);
-			string oauthToken = UrlEncode("oauth_token", _settings.AccessToken);
-			string oauthVersion = UrlEncode("oauth_version", "1.0");
-			string status = UrlEncode("status", statusMessage);
-
-			string parameterString = $"{consumerKey}&{nonce}&{signatureMethod}&{oauthTimestamp}&{oauthToken}&{oauthVersion}&{status}";
-
-			baseString = $"{baseString}&{Uri.EscapeDataString(parameterString)}";
-
-			return baseString;
-		}
-
-		private string CalculateSignature(string baseString)
-		{
-			string signingKey = $"{Uri.EscapeDataString(_settings.ConsumerSecret)}&{Uri.EscapeDataString(_settings.TokenSecret)}";
-
-			using HMACSHA1 hmac = new (Encoding.UTF8.GetBytes(signingKey));
-			byte[] hashedBytes = hmac.ComputeHash(Encoding.UTF8.GetBytes(baseString));
-
-			return Convert.ToBase64String(hashedBytes);
-		}
-
-		private string CalculateOAuthAuthorizationString(string nonce, string timestamp, string signature)
-		{
-			var authorizationString = new StringBuilder();
-
-			authorizationString.Append(KVPair("oauth_consumer_key", _settings.ConsumerKey, false));
-			authorizationString.Append(KVPair("oauth_token", _settings.AccessToken, false));
-			authorizationString.Append(KVPair("oauth_signature_method", "HMAC-SHA1", false));
-			authorizationString.Append(KVPair("oauth_timestamp", timestamp, false));
-			authorizationString.Append(KVPair("oauth_nonce", nonce, false));
-			authorizationString.Append(KVPair("oauth_version", "1.0", false));
-			authorizationString.Append(KVPair("oauth_signature", signature, true));
-
-			return authorizationString.ToString();
-		}
-
-		private static string UrlEncode(string left, string right)
-		{
-			return $"{Uri.EscapeDataString(left)}={Uri.EscapeDataString(right)}";
-		}
-
-		private static string KVPair(string left, string right, bool final)
-		{
-			return $"{Uri.EscapeDataString(left)}=\"{Uri.EscapeDataString(right)}\"{(final ? "" : ", ")}";
-		}
-
-		private string GenerateNonce()
-		{
-			string nonceCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-			var outputString = new StringBuilder();
-			for (int i = 0; i < 32; ++i)
+			public TwitterPost(string text)
 			{
-				outputString.Append(nonceCharacters.Substring(Rng.Next(0, nonceCharacters.Length), 1));
+				this.Text = text;
 			}
 
-			return outputString.ToString();
+			public TwitterPost(IPostable post)
+			{
+				this.Text = $"{post.Announcement}{Environment.NewLine}{post.Link}";
+			}
 		}
 	}
 }
